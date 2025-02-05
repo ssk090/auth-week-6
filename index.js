090dd8f9a97e1b2e741884f6da2e95ae79c68412
ssk090 <shivanandasai.38@gmail.com>
2025-02-05 22:48:32 +0530

    initialize project with Express server and user authentication endpoints

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const users = [];
+
+function generateToken() {
+    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    let token = "";
+    for (let i = 0; i < 32; i++) {
+        // use a simple function here
+        token += options[Math.floor(Math.random() * options.length)];
+    }
+    return token;
+}
+
+app.post('/signup', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    users.push({
+        username, password
+    })
+
+    res.json({
+        message: "You are signed up!"
+    })
+    console.log(users)
+})
+
+
+app.post('/signin', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = users.find(user => user.username === username && user.password === password)
+
+    if (user) {
+        const token = generateToken();
+        user.token = token;
+        res.json({
+            token
+        })
+    } else {
+        res.status(403).send({
+            message: "Invalid username or password"
+        })
+    }
+    console.log(users)
+
+})
+
+app.listen(3000, () => {
+    console.log("Server Running on port 3000");
+})
