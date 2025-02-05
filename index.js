6e21e4ec81523744675306abb2b6f3fcf6fe1d14
ssk090 <shivanandasai.38@gmail.com>
2025-02-05 23:37:36 +0530

    implement JWT authentication for user signup and signin

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 const express = require('express');
+const jwt = require('jsonwebtoken')
 
 const app = express();
 app.use(express.json());
 
 const users = [];
 
-function generateToken() {
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-    let token = "";
-    for (let i = 0; i < 32; i++) {
-        // use a simple function here
-        token += options[Math.floor(Math.random() * options.length)];
-    }
-    return token;
-}
+const JWT_SECRET = "shiva";
 
 app.post('/signup', (req, res) => {
     const username = req.body.username;
@@ -38,27 +30,30 @@ app.post('/signin', (req, res) => {
     const user = users.find(user => user.username === username && user.password === password)
 
     if (user) {
-        const token = generateToken();
-        user.token = token;
+        const token = jwt.sign({
+            username: user.username
+        }, JWT_SECRET)
+        user.token = token
         res.json({
             token
         })
+        console.log(users)
     } else {
         res.status(403).send({
             message: "Invalid username or password"
         })
     }
-    console.log(users)
-
 })
 
 app.get('/me', (req, res) => {
     const token = req.headers.authorization;
-    const user = users.find(user => user.token === token)
 
-    if (user) {
+    const decodedInformation = jwt.verify(token, JWT_SECRET)
+    const username = decodedInformation.username;
+
+    if (username) {
         res.json({
-            username: user.username
+            username
         })
     } else {
         res.status(401).send({
